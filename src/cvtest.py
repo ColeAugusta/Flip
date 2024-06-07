34e3f31287f4624efb6947f0ff5c36c90d1116d7
augusc2 <augusc2@rpi.edu>
2024-06-07 14:45:07 -0400

    Added video capture, issues with color

--- src/cvtest.py
+++ src/cvtest.py
@@ -2,11 +2,24 @@ import numpy as np
 import cv2 as cv
 
 vid_capture = cv.VideoCapture(0, cv.CAP_DSHOW)
+
 if vid_capture.isOpened() == False:
     print("Error opening the video file")
-else:
-    frame_width = int(vid_capture.get(3))
-    frame_height = int(vid_capture.get(4))
-    frame_size = (frame_width,frame_height)
-    fps = 20
-    
+    exit()
+
+frame_width = int(vid_capture.get(3))
+frame_height = int(vid_capture.get(4))
+frame_size = (frame_width,frame_height)
+fps = 20
+
+while True:
+    ret, frame = vid_capture.read()
+    if not ret:
+        print("Not receiving frame, exiting\n")
+        break
+
+    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB);
+    cv.imshow('frame', image)
+
+vid_capture.release()
+cv.destroyAllWindows()

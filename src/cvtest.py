5310ea306f846326e66a39885d349809b49d53e8
katijino <powers2@rpi.edu>
2024-06-11 14:46:36 -0400

    Added hand recognition features
    utilizing an additional library called mediapipe, wrote  code to recognize Hands in a live video. Recognizes at decently high rate, but no code for "doing" anything with the hand being recognized

--- src/cvtest.py
+++ src/cvtest.py
@@ -1,25 +1,55 @@
-import numpy as np
-import cv2 as cv
+import cv2
+import mediapipe as mp
+import time
 
-vid_capture = cv.VideoCapture(0, cv.CAP_DSHOW)
 
-if vid_capture.isOpened() == False:
-    print("Error opening the video file")
-    exit()
 
-frame_width = int(vid_capture.get(3))
-frame_height = int(vid_capture.get(4))
-frame_size = (frame_width,frame_height)
-fps = 20
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
-    ret, frame = vid_capture.read()
-    if not ret:
-        print("Not receiving frame, exiting\n")
+    #reads the image
+    success, img = cap.read()
+    #change color to RGB for detection using mediapipe
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #processes each image
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    #if there are results, if they find some sort of hand.
+    if results.multi_hand_landmarks:
+        #for every "point" of the hand in the results
+        for handLms in results.multi_hand_landmarks:
+            #for every point, there is an id and a location of the point of the hand. this loops through all of them. 
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w), int(lm.y*h)
+                #if id ==0:
+                #makes a small circle at each "point" of the hand
+                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+            #actually draws the circle on the image.
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    #gets FPS
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    #puts FPS in image
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    #shows image
+    cv2.imshow("Image", img)
+    #lets user press "q" button to exit program
+    if cv2.waitKey(1) & 0xFF == ord('q'):#close using q
         break
 
-    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB);
-    cv.imshow('frame', image)
-
-vid_capture.release()
-cv.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
